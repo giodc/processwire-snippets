dd77986d4ca540a0512840c986338c60ef4a7226
Giovanni De Carlo <info@giodc.com>
2017-07-11 15:02:28 -0500

    first upload

---
+++ get-title-and-get-parent-title.php
@@ -0,0 +1,18 @@
+
+
+<?php
+
+// ProcessWire - uikit3 profile 
+
+
+//  parent title
+
+ echo ukHeading1(page()->parent->title, 'divider');
+ 
+ 
+//  title
+ 
+echo ukHeading1(page()->title, 'divider'); ?>
+ 
+ 
+  ?>
